860329694dd0c940e405e4f97b5da5e7b071d256
kamran6433 <kamran_12@icloud.com>
2023-05-17 11:48:44 +0100

    add views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import MinecraftUser, Item
 from .forms import CreateAccount
 
@@ -10,19 +10,38 @@ def base_page(reponse, id):
     dynamic_dict = {"name": ls.name}
     return render(reponse, "base.html", dynamic_dict)
 
+
 def home_page(request):
     return render(request, "home.html", {})
 
+
 def error_page(request):
     return render(request, "error.html")
+
 
 def profile_page(reponse, id):
     ls = MinecraftUser.objects.get(id=id)
     return render(reponse, "profile.html", {"ls": ls})
 
+
 def create_account_page(response):
-    form = CreateAccount()
+    # Post is a high security request which hides the information. Get has less security and anyone can see the information.
+    if response.method == "POST":
+        form = CreateAccount(response.POST)
+
+        if form.is_valid():
+            # This decrypts the data
+            n = form.cleaned_data["name"]
+            t = MinecraftUser(name=n)
+            t.save()
+        
+        return HttpResponseRedirect("/%i" %t.id)
+
+    else:
+        form = CreateAccount()
+    
     return render(response, "create_account.html", {"form": form})
+
 
 # Dynamic page
 def user_input_int_page(response, id):
